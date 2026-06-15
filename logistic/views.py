2402697ce4a6ab051bb124631371a6653b053fc7
Aleksandr Muzhev <a.muzhev@adengi.ru>
2026-06-15 23:40:28 +0500

    Add Deploy

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Привет ВСЕМ СТУДЕНТАМ!'})
+    return Response({'message': 'Это после добавления шага Деплой'})
